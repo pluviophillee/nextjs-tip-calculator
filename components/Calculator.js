fcf12efed95b854ad12b69e17d57ad117dcead15
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-06-11 09:00:01 +0300

    Completed assignment

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Calculator = () => {
     // TODO: start coding here!
+    const [totalBill, setTotalBill] = useState("");
+    const [tipPercentage, setTipPercentage] = useState("");
+    const [numberOfPeople, setNumberOfPeople] = useState("");
+    const [finalTipAmount, setFinalTipAmount] = useState(null);
+    const [finalPrice, setFinalPrice] = useState(null);
 
+    useEffect(() => {
+        if (totalBill !== "" && tipPercentage !== "" && numberOfPeople !== "") {
+            const bill = parseFloat(totalBill);
+            const tip = parseFloat(tipPercentage);
+            const people = parseFloat(numberOfPeople);
+
+            if (bill > 0 && tip > 0 && people > 0) {
+                const tipByPerson = (bill * tip / 100 / people).toFixed(2);
+                const billByPerson = ((bill + tip) / people).toFixed(2);
+                setFinalTipAmount(tipByPerson);
+                setFinalPrice(billByPerson);
+            }
+        }
+    }, [totalBill, tipPercentage, numberOfPeople]);
     return (
         <main>
             <img
@@ -23,6 +43,8 @@ const Calculator = () => {
                             placeholder="0"
                             name="Total bill value"
                             id="totalBill"
+                            value={totalBill}
+                            onChange={(e) => setTotalBill(parseFloat(e.target.value))}
                         />
                     </div>
 
@@ -33,18 +55,18 @@ const Calculator = () => {
                                 valid</small>
                         </div>
                         <div className="input-tips-container">
-                            <button className="body-l-text input-tip" id="tip5">5%
+                            <button className="body-l-text input-tip" id="tip5" value={5} onClick={(e) => setTipPercentage(parseFloat(e.target.value))}>5%
                             </button>
-                            <button className="body-l-text input-tip" id="tip10">10%
+                            <button className="body-l-text input-tip" id="tip10" value={10} onClick={(e) => setTipPercentage(parseFloat(e.target.value))}>10%
                             </button>
-                            <button className="body-l-text input-tip" id="tip15">15%
+                            <button className="body-l-text input-tip" id="tip15" value={15} onClick={(e) => setTipPercentage(parseFloat(e.target.value))}>15%
                             </button>
-                            <button className="body-l-text input-tip" id="tip25">25%
+                            <button className="body-l-text input-tip" id="tip25" value={25}  onClick={(e) => setTipPercentage(parseFloat(e.target.value))}>25%
                             </button>
-                            <button className="body-l-text input-tip" id="tip50">50%
+                            <button className="body-l-text input-tip" id="tip50" value={50} onClick={(e) => setTipPercentage(parseFloat(e.target.value))}>50%
                             </button>
                             <input type="number" className="body-l-text input-field" placeholder="Custom"
-                                   id="totalTipPercentage"></input>
+                                   id="totalTipPercentage" onChange={(e) => setTipPercentage(e.target.value)}></input>
                         </div>
                     </div>
 
@@ -60,8 +82,8 @@ const Calculator = () => {
                             placeholder="0"
                             name="Number of people"
                             id="numberOfPeople"
-
-                        />
+                            value={numberOfPeople}
+                            onChange={(e) => setNumberOfPeople(parseFloat(e.target.value))}/>
                     </div>
                 </div>
                 <div className="card-right">
@@ -70,14 +92,15 @@ const Calculator = () => {
                             <b className="body-text card-price-title">Tip Amount</b>
                             <p className="body-s-text card-price-subtitle">/ person</p>
                         </div>
-                        <strong className="strong-text card-price-value" id="tipAmount">$0.00</strong>
+                        <strong className="strong-text card-price-value" id="tipAmount">{finalTipAmount > 0 ? <p>{finalTipAmount}</p>: <p>$0.00</p>}</strong>
                     </section>
                     <section className="card-price-container">
                         <div>
                             <b className="body-text card-price-title">Total</b>
                             <p className="body-s-text card-price-subtitle">/ person</p>
                         </div>
-                        <strong className="strong-text card-price-value" id="totalPrice">$0.00</strong>
+                        <strong className="strong-text card-price-value" id="totalPrice" value>{finalPrice > 0 ? <p>${finalPrice}</p> : <p>$0.00</p>}
+</strong>
                     </section>
                     <button className="btn btn-primary btn-reset">Reset</button>
                 </div>
